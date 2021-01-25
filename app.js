3c2840ccfcf000d22ef2c3f2a717657057147e7e
Muhammad Waqas Rizvi <waqashafiz90@gmail.com>
2021-01-26 00:35:22 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,14 @@
+function input(e) {
+    var a = document.getElementById("res")
+    a.value += e
+}
+function calculation() {
+    var a = document.getElementById("res")
+    var cal = a.value
+    var final = eval(cal)
+    a.value = final
+}
+function clear() {
+    var a = document.getElementById('res')
+    a.value = ''
+}
